6f0f63c2416f220dc9f8d7f2911784c0b8461ed5
Gonzalo Vásquez <gvasquez@altiuz.cl>
2017-12-15 14:58:04 -0300

    unused imports

--- src/main/java/cl/waypoint/ms/mailer/rest/ExceptionHandlerController.java
+++ src/main/java/cl/waypoint/ms/mailer/rest/ExceptionHandlerController.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
