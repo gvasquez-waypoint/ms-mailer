162f95f805c632026c1abdcea0b4b0164c1f172f
Gonzalo Vásquez <gvasquez@waypoint.cl>
2018-02-01 16:53:24 -0300

    se cambian arrays a lists para facilitar validación

--- src/main/java/cl/waypoint/ms/mailer/dto/Message.java
+++ src/main/java/cl/waypoint/ms/mailer/dto/Message.java
@@ -1,22 +1,25 @@
 package cl.waypoint.ms.mailer.dto;
 
 import java.util.Arrays;
+import java.util.List;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+import cz.jirutka.validator.collection.constraints.EachPattern;
+
 public class Message {
 	
-	private static final String EMAIL_REGEX = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\\\.[A-Z]{2,6}$";
+	private static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
 
-	@Pattern(regexp = EMAIL_REGEX, flags = Pattern.Flag.CASE_INSENSITIVE)
-	private String[] to;
+	@EachPattern(regexp = EMAIL_REGEX, flags = Pattern.Flag.CASE_INSENSITIVE)
+	private List<String>to;
 	
-	@Pattern(regexp = EMAIL_REGEX, flags = Pattern.Flag.CASE_INSENSITIVE)
-	private String[] cc;
+	@EachPattern(regexp = EMAIL_REGEX, flags = Pattern.Flag.CASE_INSENSITIVE)
+	private List<String> cc;
 	
-	@Pattern(regexp = EMAIL_REGEX, flags = Pattern.Flag.CASE_INSENSITIVE)
-	private String[] bcc;
+	@EachPattern(regexp = EMAIL_REGEX, flags = Pattern.Flag.CASE_INSENSITIVE)
+	private List<String> bcc;
 	
 	@NotNull
 	@Pattern(regexp = EMAIL_REGEX, flags = Pattern.Flag.CASE_INSENSITIVE)
@@ -32,29 +35,6 @@ public class Message {
 	private String body;
 	private Attachment[] attachments;
 
-	public String[] getTo() {
-		return to;
-	}
-
-	public void setTo(String[] to) {
-		this.to = to;
-	}
-
-	public String[] getCc() {
-		return cc;
-	}
-
-	public void setCc(String[] cc) {
-		this.cc = cc;
-	}
-
-	public String[] getBcc() {
-		return bcc;
-	}
-
-	public void setBcc(String[] bcc) {
-		this.bcc = bcc;
-	}
 
 	public String getFrom() {
 		return from;
@@ -98,9 +78,34 @@ public class Message {
 
 	@Override
 	public String toString() {
-		return "Message [to=" + Arrays.toString(to) + ", cc=" + Arrays.toString(cc) + ", bcc=" + Arrays.toString(bcc)
-				+ ", from=" + from + ", replyTo=" + replyTo + ", subject=" + subject + ", body=" + body
-				+ ", attachments=" + Arrays.toString(attachments) + "]";
+		return "Message [to=" + to + ", cc=" + cc + ", bcc=" + bcc + ", from=" + from + ", replyTo=" + replyTo
+				+ ", subject=" + subject + ", body=" + body + ", attachments=" + Arrays.toString(attachments) + "]";
 	}
+
+	public List<String> getTo() {
+		return to;
+	}
+
+	public void setTo(List<String> to) {
+		this.to = to;
+	}
+
+	public List<String> getCc() {
+		return cc;
+	}
+
+	public void setCc(List<String> cc) {
+		this.cc = cc;
+	}
+
+	public List<String> getBcc() {
+		return bcc;
+	}
+
+	public void setBcc(List<String> bcc) {
+		this.bcc = bcc;
+	}
+
+
 
 }
